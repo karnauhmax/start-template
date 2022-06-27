be5303a71c20f39ab77a3bd71fa4eb9d08a3fb2d
Max <djentmx@gmail.com>
2022-06-27 14:41:14 +0300

    added dynamic adapt script

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,4 @@ import Modal from "./functions/modals";
 import Tab from "./functions/tabs";
 import { burger } from "./functions/burger";
 import Splide from "@splidejs/splide";
+import DynamicAdapt from "./functions/dynamicAdapt";
